8cc463f1e2fac939542dd4bfc8c313665e66c1b6
nipaaa <akternipa941@gmail.com>
2023-05-15 17:04:54 +0600

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,24 +33,29 @@ const Header = () => {
 
   const initialPlaceholder =
     countryOptions.length > 0 ? countryOptions[0].label : "";
-    const customStyles = {
-      control: (provided) => ({
-        ...provided,
-        border: 'none',
-        boxShadow: 'none',
-        borderRadius: 0,
-        width:"120px"
-      }),
-      option: (provided, state) => ({
-        ...provided,
-        backgroundColor: state.isSelected ? 'transparent' : provided.backgroundColor,
-        color: state.isSelected ? 'inherit' : provided.color,
-      }),
-      singleValue: (provided, state) => ({
-        ...provided,
-        color: state.isSelected ? 'inherit' : provided.color,
-      }),
-    };
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      border: "none",
+      boxShadow: "none",
+      borderRadius: 0,
+      width: "120px",
+      borderRadius: "15px",
+      backgroundColor: "#E1E6F2",
+      padding:"5px 0"
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected
+        ? "transparent"
+        : provided.backgroundColor,
+      color: state.isSelected ? "inherit" : provided.color,
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color: state.isSelected ? "inherit" : provided.color,
+    }),
+  };
 
   return (
     <div className="">
